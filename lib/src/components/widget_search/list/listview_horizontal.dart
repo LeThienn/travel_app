dee796919f7fbec1841df8ef07184f5f0af29740
LeThienIT <55796074+LeThienn@users.noreply.github.com>
2021-12-24 21:46:45 +0700

    update model travel

--- lib/src/components/widget_search/list/listview_horizontal.dart
+++ lib/src/components/widget_search/list/listview_horizontal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/src/components/widget_search/list/card/card_travel.dart';
+import 'package:travel_app/src/resource/model/travel.dart';
 
 class ListViewHorizontalTravel extends StatefulWidget {
   const ListViewHorizontalTravel({Key? key}) : super(key: key);
@@ -10,42 +10,76 @@ class ListViewHorizontalTravel extends StatefulWidget {
 }
 
 class _ListViewHorizontalTravelState extends State<ListViewHorizontalTravel> {
+  final List<Travel> travel = [
+    Travel(name: "Lighthouse", location: "Edinburgh"),
+    Travel(name: "Eiffel tower", location: "Paris"),
+    Travel(name: "Big Ben", location: "London"),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-      margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
-      height: 300, // chieu dai cua tung card
-      child: ListView(
+    return ListView.builder(
+        itemCount: travel.length,
         scrollDirection: Axis.horizontal,
-        children: [
-          _buildCardTravel(),
-          const SizedBox(
-            width: 20,
-          ),
-          _buildCardTravel(),
-          const SizedBox(
-            width: 20,
-          ),
-          _buildCardTravel(),
-           const SizedBox(
-            width: 20,
-          ),
-          _buildCardTravel(),
-           const SizedBox(
-            width: 20,
-          ),
-          _buildCardTravel(),
-           const SizedBox(
-            width: 20,
-          ),
-          _buildCardTravel()
-        ],
-      ),
-    );
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: const EdgeInsets.only(left: 15, top: 15),
+            height: 300, // chieu dai cua tung card
+            child: SizedBox(
+                width: 170.0,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                            color: Colors.blue),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 10,
+                                height: 35,
+                              ),
+                              Text(
+                                travel[index].name,
+                                style: const TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 7,
+                              ),
+                              const Icon(
+                                Icons.location_on,
+                                color: Colors.blue,
+                              ),
+                              Text(
+                                travel[index].location,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          );
+        });
+    // );
   }
-}
-
-Widget _buildCardTravel() {
-  return const CardTravel();
 }
---
+++ lib/src/resource/model/travel.dart
@@ -0,0 +1,5 @@
+class Travel {
+  String name;
+  String location;
+  Travel({required this.name, required this.location});
+}
